3a0ae30a2547b8ffc788c84ab14955cb468da558
gabrielhastec <gabrielhastec.dev@gmail.com>
2026-02-02 22:27:15 -0300

    First Commit: Primeira aula de java, passos básicos da linguagem.

---
+++ aulas/Aula 01 - Introdução/Helloword.java
@@ -0,0 +1,23 @@
+
+// Todo programa Java começa com uma classe
+// O nome da classe deve ser o mesmo nome do arquivo (HelloWorld.java)
+
+public class HelloWorld {
+
+    // O método main é o ponto de entrada do programa
+    // É por aqui que o Java começa a executar o código
+
+    public static void main(String[] args) {
+
+        // System.out.print imprime um texto na tela
+        // Sem pular linha no final
+        System.out.print("Olá, mundo!");
+
+        // System.out.println imprime um texto na tela
+        // E pula uma linha depois de imprimir
+        System.out.println(" Meu primeiro programa em Java");
+
+        // Comentários servem para explicar o código
+        // O Java ignora tudo que está depois de //
+    }
+}
