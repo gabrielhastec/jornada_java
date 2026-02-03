146783fd4d91919c968a4f8f0da6effa7a0eb817
gabrielhastec <gabrielhastec.dev@gmail.com>
2026-02-02 22:41:59 -0300

    Aula 02: Operadores Aritméticos.

---
+++ aulas/Aula 02 - Op_Aritméticos/op_arit.java
@@ -0,0 +1,26 @@
+
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+
+        // Operador de SOMA (+)
+        // Soma dois números
+        System.out.println("Soma: 5 + 3 = " + (5 + 3));
+
+        // Operador de SUBTRAÇÃO (-)
+        // Subtrai um número do outro
+        System.out.println("Subtração: 10 - 4 = " + (10 - 4));
+
+        // Operador de MULTIPLICAÇÃO (*)
+        // Multiplica dois números
+        System.out.println("Multiplicação: 6 * 2 = " + (6 * 2));
+
+        // Operador de DIVISÃO (/)
+        // Divide um número pelo outro
+        System.out.println("Divisão: 8 / 2 = " + (8 / 2));
+
+        // Operador de RESTO (%)
+        // Retorna o resto da divisão
+        System.out.println("Resto da divisão: 10 % 3 = " + (10 % 3));
+    }
+}
