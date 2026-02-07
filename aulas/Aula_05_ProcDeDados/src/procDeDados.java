104e1ce52d2e63f6eaa96dceddea2ca6541873c9
gabrielhastec <gabrielhastec.dev@gmail.com>
2026-02-06 22:22:36 -0300

    Chore(aulas): Adiciona a aula 05 - Processamento de Dados e Casting.

---
+++ aulas/Aula_05_ProcDeDados/src/procDeDados.java
@@ -0,0 +1,34 @@
+
+public class procDeDados {
+    public static void main(String[] args) throws Exception {
+
+        int x1, x2;
+
+        x1 = 10;
+        x2 = 2 * x1;
+
+        System.out.println(x1);
+        System.out.println(x2);
+
+        double b, B, h, area;
+
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+        area = (b + B) / 2.0 * h;
+
+        System.out.println(area);
+
+        // Casting
+
+        int a1, a2;
+        double resultado;
+
+        a1 = 5;
+        a2 = 2;
+        resultado = (double) a1 / a2;
+
+        System.out.println(resultado);
+
+    }
+}
