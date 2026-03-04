e77dd8678814b0dd880948ebf0bdc2e9944cb307
gabrielhastec <gabrielhastec.dev@gmail.com>
2026-03-04 00:04:23 -0300

    Feat(exer): Correção de sintaxe no exercicio 04.

--- exercicios/Exer_04_Salario_2/src/application/Main.java
+++ exercicios/Exer_04_Salario_2/src/application/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc      = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         FolhaService folhaService = new FolhaService();
 
         Menu.exibirCabecalho();
