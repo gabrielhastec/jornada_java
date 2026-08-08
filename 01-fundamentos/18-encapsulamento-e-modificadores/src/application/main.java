0b678bf2d002610a56507fb4112b0d8d2dcbb6ac
gabrielhastec <gabrielhastec.dev@gmail.com>
2026-08-07 23:22:49 -0300

    fix(aulas): corrige a capitalização do método main para seguir a convenção Java

--- 01-fundamentos/18-encapsulamento-e-modificadores/src/application/main.java
+++ 01-fundamentos/18-encapsulamento-e-modificadores/src/application/main.java
@@ -3,7 +3,8 @@ package application;
 import entities.Produto;
 
 public class Main {
-    public static void Main(String[] args) {
+
+    public static void main(String[] args) {
         Produto p = new Produto("Notebook", 2500.0, 10);
         System.out.println("Produto: " + p.getNome());
         System.out.println("Preço: R$" + p.getPreco());
